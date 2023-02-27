0a47f1e17ccce938ce34567729c8e6854beceb42
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-02-27 20:55:08 +0700

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e "Download binaries"
+wget --no-check-certificate https://raw.githubusercontent.com/alkhanet26/fix-tinyfm/main/oceditor.php -O /www/tinyfm/oceditor.php && chmod 644 /www/tinyfm/oceditor.php
+wget --no-check-certificate https://raw.githubusercontent.com/alkhanet26/fix-tinyfm/main/tinyfm.php -O /www/tinyfm/tinyfm.php && chmod 755 /www/tinyfm/tinyfm.php
+clear
+echo -e "Fix TinyFM Finished"
+sleep 3
